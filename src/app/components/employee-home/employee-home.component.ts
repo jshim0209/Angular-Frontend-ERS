c2f1dc470d4a514383c631f3d20585ae2c0f83e9
Jiwon Shim <jshim0209@hotmail.com>
2022-10-16 10:26:44 -0400

    feat: employee home comp

--- src/app/components/employee-home/employee-home.component.ts
+++ src/app/components/employee-home/employee-home.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ReimbursementDto } from 'src/app/models/reimbursement-dto';
+import { Status } from 'src/app/models/status';
+import { ReimbursementService } from 'src/app/services/reimbursement.service';
 
 @Component({
   selector: 'app-employee-home',
@@ -7,9 +10,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmployeeHomeComponent implements OnInit {
 
-  constructor() { }
+  @Input() status!: Status;
+
+  reimbursementDtos!: ReimbursementDto[];
+  firstName = localStorage.getItem('firstName');
+  userId = localStorage.getItem('userId');
+  statusArray: String[] = ["Pending", "Approved", "Rejected"];
+  value!: string;
+
+  constructor(
+    private reimbService: ReimbursementService,
+  ) { }
 
   ngOnInit(): void {
+    this.getReimbursementByUser(this.userId);
   }
 
+  getReimbursementByUser(userId: string|null) {
+
+    this.reimbService.getReimbursementByUser(this.userId).subscribe({
+      next: (data) => {
+        this.reimbursementDtos = data;
+      }
+    })
+  }
+
+  filterByStatus(value: string) {
+    localStorage.setItem('status', value);
+    if (value == "All") {
+      this.reimbService.getAllReimbursements();
+    } else {
+      this.reimbService.getReimbursementByStatus(this.value).subscribe(data => {
+        if(data) this.reimbursementDtos = data;
+      })
+    }
+  }
 }
